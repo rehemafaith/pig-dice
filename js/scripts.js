3704a109bd21063b005a7c837ae16c4b6b411692
rehemafaith <rehemafaith01@gmail.com>
2019-03-04 12:34:56 +0300

    add features to the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,87 @@
+//business logic
+function Name (name , names ){
+  this.name=name
+  this.names=names
+}
+var result = 0
+var score = 0
+var currentscore=0
+function off(){
+  document.getElementById('btn1').disabled=true;
+  document.getElementById('btn3').disabled=false;
+}
+function on(){
+  document.getElementById('btn3').disabled=true;
+  document.getElementById('btn1').disabled=false;
+
+}
+
 //user logic
 
-result = 0
-score = 0
+$(document).ready(function(event){
+  $(".btn").click(function(){
+    $(".game").show();
+    $(".intro").hide();
+    var player1=new Name(this.name=$("input#plyr1").val(),this.names=$("input#plyr2").val());
 
-$(document).ready(function(){
-  $("#btn1").click(function(){
+    document.getElementById('namep1').innerHTML = player1.name
+
+    document.getElementById('namep2').innerHTML = player2.name
+  })
+    $("#btn1").click(function(){
+
     var player1Dice = Math.floor((Math.random()*6)+1);
+    if (player1Dice === 1) {
+       currentscore-=currentscore
+          document.getElementById('return1').innerHTML = currentscore  ;
+          off()
+    }
     if (player1Dice > 1){
-      result+=player1Dice
-      document.getElementById('namep1').innerHTML = result
+       currentscore +=player1Dice
+      document.getElementById('return1').innerHTML = currentscore
     }
-    else if (player1Dice === 1) {
-          document.getElementById('namep1').innerHTML = player1Dice -= result;
-    }
-    if (result === 100) {
-      score+=result
-      document.getElementById('namep1').innerHTML = score
 
-    }
-   $("#btn2").click(function(){
 
-   })
+    $("#btn2").click(function(){
+
+        score+=currentscore
+        if (score === 100) {
+        document.getElementById('return1').innerHTML = "congratulations You have won!Your score is" + score
+
+      }else{
+        currentscore=0
+        off()
+      }
+      document.getElementById('return1').innerHTML = score
+    })
   });
-  $("#btn3").click(function(){
-    var player1Dice = Math.floor((Math.random()*7)+1);
-    if (player1Dice == 1) {
-      document.getElementById('namep2').innerHTML = 0
-    }
 
-
-  });
- //$("#btn1").click(function(){
-  //  document.getElementById('btn3').disabled = true
-  //  document.getElementById('btn1').disabled = false
-//  $("#btn2").click(function(){
-  //  document.getElementById('btn1').diabled = true
 
   })
+    $("#btn3").click(function(){
+
+    var player1Dice = Math.floor((Math.random()*6)+1);
+    if (player1Dice === 1) {
+       currentscore-=currentscore
+          document.getElementById('return2').innerHTML = currentscore  ;
+          on()
+    }
+    if (player1Dice > 1){
+       currentscore +=player1Dice
+      document.getElementById('return2').innerHTML = currentscore
+    }
+
+
+    $("#btn4").click(function(){
+
+        score+=currentscore
+        if (score === 100) {
+        document.getElementById('return2').innerHTML = "congratulations You have won!Your score is" + score
+
+      }else{
+        currentscore=0
+        on()
+      }
+      document.getElementById('return1').innerHTML = score
+    })
+  });
